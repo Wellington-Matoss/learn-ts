177b88f71a89ec71d4d8101d79b2a4e337d4c71a
Wellington-Matoss <wellington.matos.neves@gmail.com>
2026-03-04 05:57:44 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,27 @@
+function soma(a: number, b: number): number {
+  return a + b;
+}
+
+// console.log(soma(1, 1));
+
+const name: string = 'Kyoya';
+const age: number = 26;
+const single: boolean = false;
+
+// console.log(name, typeof name);
+// console.log(age, typeof age);
+// console.log(single, typeof single);
+
+const favoriteFoods: string[] = ['Cachorro-quente', 'Vatapá', 'Açaí'];
+const favoriteNumbers: number[] = [2, 7, 12];
+
+// favoriteFoods.map((food) => console.log(food, typeof food));
+// favoriteNumbers.map((num) => console.log(num, typeof num));
+
+const x: any = { name: 'aa' };
+const y: string | undefined = undefined;
+const z: string | null = null;
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
